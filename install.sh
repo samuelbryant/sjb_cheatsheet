33c0b8b27dda4c5162876a924f1dad8c47b84813
sambryant <samuel.bryant@yale.edu>
2018-05-18 13:19:42 -0400

    Fixed bugs in install script and upgraded its functionality. Fixes #33 and fixes #34

--- install.sh
+++ install.sh
@@ -2,15 +2,36 @@
 # Library settings
 ## Machine install location
 #DEFAULT_LIB_LOCATION="/usr/local/lib/python3.6/dist-packages"
-## Local install location (use until ready for prod)
-DEFAULT_LIB_LOCATION="$HOME/.local/lib/python3.6/site-packages"
+
 SUITE_LIB_NAME="sjb"
 APP_LIB_NAME="cs"
 COMMON_LIB_NAME="common"
+FRONTEND_NAME="sjb-cheatsheet"
+
+# Library install locations
+SITE_LIB_LOC="/usr/local/lib/python3.6/dist-packages"
+USER_LIB_LOC="$HOME/.local/lib/python3.6/site-packages"
 
 # Frontend settings
-TARGET_FRONTEND_SCRIPT="/usr/local/bin/sjb-cheatsheet"
-LOCAL_FRONTEND_SCRIPT="./bin/sjb-cheatsheet"
+SITE_FRONTEND_TARGET="/usr/local/bin/${FRONTEND_NAME}"
+USER_FRONTEND_TARGET="$HOME/bin/${FRONTEND_NAME}"
+
+# Local sources
+local_frontend="./bin/${FRONTEND_NAME}"
+local_suite_lib="./src/$SUITE_LIB_NAME"
+local_app_lib="./src/$SUITE_LIB_NAME/$APP_LIB_NAME"
+local_common_lib="./src/$SUITE_LIB_NAME/$COMMON_LIB_NAME"
+
+RED='\033[0;41m'
+NC='\033[0m' # No Color
+
+function wrnmsg {
+  echo -e "${RED}Warning:${NC} $1" > /dev/stderr
+}
+
+function errmsg {
+  echo -e "${RED}Error:${NC} $1" > /dev/stderr
+}
 
 # Make sure we are not running as sudo user:
 if [ "$EUID" -eq 0 ]; then
@@ -25,80 +46,173 @@ if [ "$?" -ne "0" ]; then
   exit 2
 fi
 
-# Prompt user for the python library location
-printf "Enter library directory intall directory (default is ${DEFAULT_LIB_LOCATION}): "
-read location
-if [[ "$location"=="" ]]; then
-  location="$DEFAULT_LIB_LOCATION"
+# Check that local sources exist.
+if [ ! -f "$local_frontend" ]; then
+  errmsg "Local frontend script not found: '$local_frontend'"
+  exit 1
+elif [ ! -d "$local_suite_lib" ]; then
+  errmsg "Local suite library not found: '$local_suite_lib'"
+  exit 1
+elif [ ! -d "$local_app_lib" ]; then
+  errmsg "Local app library not found: '$local_app_lib'"
+  exit 1
+elif [ ! -d "$local_common_lib" ]; then
+  errmsg "Local common library not found: '$local_common_lib'"
+  exit 1
+fi
+
+# Prompt user to get library install location.
+printf "Where would you like to install the python libraries?\n"
+printf "\t1 (user) ${USER_LIB_LOC}\n"
+printf "\t2 (site) ${SITE_LIB_LOC}\n"
+read -p "Choose (default is 1): " choice
+if [[ "$choice" == "1" ]] || [[ "$choice" == "" ]]; then
+  lib_location="${USER_LIB_LOC}"
+  lib_is_sitewide=0
+elif [[ "$choice" == "2" ]]; then
+  lib_location="${SITE_LIB_LOC}"
+  lib_is_sitewide=1
+else
+  errmsg "Bad response"
+  exit 1
+fi
+
+# Prompt user to get binary install location
+printf "Where would you like to install the frontend script?\n"
+printf "\t1 (user) ${USER_FRONTEND_TARGET}\n"
+printf "\t2 (site) ${SITE_FRONTEND_TARGET}\n"
+read -p "Choose (default is 2): " choice
+if [[ "$choice" == "1" ]]; then
+  frontend_target="${USER_FRONTEND_TARGET}"
+  frontend_is_sitewide=0
+elif [[ "$choice" == "2" ]] || [[ "$choice" == "" ]]; then
+  frontend_target="${SITE_FRONTEND_TARGET}"
+  frontend_is_sitewide=1
+else
+  errmsg "Bad response"
+  exit 1
+fi
+
+target_suite_lib="${lib_location}/$SUITE_LIB_NAME"
+target_app_lib="${lib_location}/$SUITE_LIB_NAME/$APP_LIB_NAME"
+target_common_lib="${lib_location}/$SUITE_LIB_NAME/$COMMON_LIB_NAME"
+flag_copy_common_lib=1
+flag_remove_old_app_lib=0
+flag_remove_old_common_lib=0
+
+# Check that install directory exists:
+if [ ! -d "${lib_location}" ]; then
+  errmsg "Error: Install directory does not exist! ('${lib_location}')"
+  exit 1
 fi
 
 # Check that install directory is in the python library path:
-python3.6 -c "import sys; sys.exit('$location' not in sys.path)"
+python3.6 -c "import sys; sys.exit('${lib_location}' not in sys.path)"
 if [ "$?" -ne "0" ]; then
-  echo "Error: Install directory ('$location') was not found in python path (sys.path)"
-fi
-
-# Check that install directory exists:
-if [ ! -d "$location" ]; then
-  echo "Error: Install directory does not exist! ('$location')"
+  errmsg "Error: Install directory ('${lib_location}') was not found in python path (sys.path)"
   exit 1
-fi
-
-target_suite_lib="$location/$SUITE_LIB_NAME"
-target_app_lib="$location/$SUITE_LIB_NAME/$APP_LIB_NAME"
-target_common_lib="$location/$SUITE_LIB_NAME/$COMMON_LIB_NAME"
-local_suite_lib="./src/$SUITE_LIB_NAME"
-local_app_lib="./src/$SUITE_LIB_NAME/$APP_LIB_NAME"
-local_common_lib="./src/$SUITE_LIB_NAME/$COMMON_LIB_NAME"
-
-# Create suite library directory
-sudo mkdir -p "$target_suite_lib"
-
-flag_copy_common_lib=1
-flag_remove_old_app_lib=0
-
-# Check that local sources exist.
-if [ ! -f "$LOCAL_FRONTEND_SCRIPT" ]; then
-  echo "Error: Local frontend script not found: '$LOCAL_FRONTEND_SCRIPT'"
-  exit 1
-elif [ ! -d "$local_suite_lib" ]; then
-  echo "Error: Local suite library not found: '$local_suite_lib'"
-  exit 1
-elif [ ! -d "$local_app_lib" ]; then
-  echo "Error: Local app library not found: '$local_app_lib'"
-  exit 1
-elif [ ! -d "$local_common_lib" ]; then
-  echo "Error: Local common library not found: '$local_common_lib'"
-  exit 1
-fi
-
-# Check if common library is already installed and matches this library.
-if [ -e "$target_common_lib" ]; then
-  diff "$local_common_lib" "$target_common_lib" 1>/dev/null 2>/dev/null
-  if [ "$?" -ne "0" ]; then
-    echo "Error: target common library is already installed (at '$target_common_lib') but its contents are different from the common library you are trying to install (at '$local_common_lib')"
-    echo "This likely means you have previously installed a different version of some '$SUITE_LIB_NAME' app. Currently this upgrade script cannot handle this scenario and the common library is not guaranteed to be backwards compatible. I will fix this in the future."
-    echo "Goodbye."
-    exit 1
-  fi
-  flag_copy_common_lib=0
 fi
 
 # Check if app library is already installed
 if [ -e "$target_app_lib" ]; then
-  echo "Warning: previous install found. Overwriting..."
+  wrnmsg "previous install found. Overwriting..."
   flag_remove_old_app_lib=1
 fi
 
+# Check if common library is already installed and matches this library.
+if [ -e "$target_common_lib" ]; then
+  diff -r "$local_common_lib" "$target_common_lib" 1>/dev/null 2>/dev/null
+  if [ "$?" -ne "0" ]; then
+    #wrnmsg "common lib mismatch: '${local_common_lib}' vs '${target_common_lib}'"
+    wrnmsg "the target common library is already installed but its contents are different from the common library you are trying to install. If you overwrite it, it is possible it will break another $SUITE_LIB_NAME application."
+    read -p "Would you like to remove the old common library and replace it with the new one? (y/N) " resp
+    if [[ $resp =~ ^[yY](e|es)?$ ]]; then
+      flag_remove_old_common_lib=1
+      flag_copy_common_lib=1
+    elif [[ $resp =~ ^[nN][oO]?$ ]] || [[ "$resp" == "" ]]; then
+      exit 0
+    else
+      errmsg "Bad response"
+      exit 1
+    fi
+  else
+    flag_copy_common_lib=0
+  fi
+fi
 
-# All checks done. Time to actually do stuff
-if [[ "$flag_copy_common_lib" -eq "1" ]]; then
-  sudo cp -r "$local_common_lib" "$target_common_lib"
+
+########## END OF CHECKS START OF CHANGES #####################
+
+# Create suite library directory
+if [[ "$lib_is_sitewide" != "0" ]]; then
+  sudo mkdir -p "$target_suite_lib"
+else
+  mkdir -p "$target_suite_lib"
 fi
+if [ "$?" -ne "0" ]; then
+  errmsg "Something went wrong with creating the suite library"
+  exit 1
+fi
+
+# Remove old common library if required
+if [[ "$flag_remove_old_common_lib" ]]; then
+  if [[ "$lib_is_sitewide" != "0" ]]; then
+    sudo rm -rf "$target_common_lib"
+  else
+    rm -rf "$target_common_lib"
+  fi
+  if [ "$?" -ne "0" ]; then
+    errmsg "Something went wrong while removing old common lib. It is likely that the $SUITE_LIB_NAME applications may be in a broken state. Sorry."
+    exit 2
+  fi
+fi
+
+# Remove old app library if required
 if [[ "$flag_remove_old_app_lib" -eq "1" ]]; then
-  sudo rm -r "$target_app_lib"
+  if [[ "$lib_is_sitewide" != "0" ]]; then
+    sudo rm -rf "$target_app_lib"
+  else
+    rm -rf "$target_app_lib"
+  fi
+  if [ "$?" -ne "0" ]; then
+    errmsg "Something went wrong while removing old app lib. It is likely that the application is in a broken state. Sorry."
+    exit 2
+  fi
 fi
-sudo cp -r "$local_app_lib" "$target_app_lib" 
-sudo cp "$LOCAL_FRONTEND_SCRIPT" "$TARGET_FRONTEND_SCRIPT"
+
+# Copy common library
+if [[ "$flag_copy_common_lib" -eq "1" ]]; then
+  if [[ "$lib_is_sitewide" != "0" ]]; then
+    sudo cp -r "$local_common_lib" "$target_common_lib"
+  else
+    cp -r "$local_common_lib" "$target_common_lib"
+  fi
+  if [ "$?" -ne "0" ]; then
+    errmsg "Something went wrong while copying common lib. It is likely that the application is in a broken state. Sorry."
+    exit 2
+  fi
+fi
+
+# Copy app library
+if [[ "$lib_is_sitewide" != "0" ]]; then
+  sudo cp -r "$local_app_lib" "$target_app_lib" 
+else
+  cp -r "$local_app_lib" "$target_app_lib" 
+fi
+if [ "$?" -ne "0" ]; then
+  errmsg "Something went wrong while copying app lib. It is likely that the application is in a broken state. Sorry."
+  exit 2
+fi
+
+# Copy frontend script
+if [[ "$frontend_is_sitewide" != "0" ]]; then
+  sudo cp "$local_frontend" "$frontend_target"
+else
+  cp "$local_frontend" "$frontend_target"
+fi
+if [ "$?" -ne "0" ]; then
+  errmsg "Something went wrong while copying frontend script. It is likely that the application is in a broken state. Sorry."
+  exit 2
+fi
 
 echo "Installation was a success!"
